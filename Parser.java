0d4c1eca77c1e668efd85ed9cf3fa1b22ad7a196
jdwaldo <jdwaldo@gmail.com>
2014-01-23 23:11:59 -0500

    Update with comments

--- Parser.java
+++ Parser.java
@@ -9,11 +9,12 @@ public class Parser {
     public Parser() {
         equation = new ArrayList<String>();
     }
-    
+ //    Constructor for Parser. Takes an input String and turns it into an ArrayList which seperates numbers and operators while keeping it in correct order.
     public Parser(String input) {
         String numberString = "";
 	equation= new ArrayList<String>();
         for (int i = 0; i < input.length(); i++) {
+            // Searches for operators of length 3. {sin,cos,tan,cot,csc,sec,log}
 	    if ( i < input.length() -2 &&linSearch(operators, input.substring(i,i+3)) != -1) {
 		if (numberString.length() > 0) {
 		    equation.add(numberString);
@@ -22,6 +23,7 @@ public class Parser {
 		equation.add(operators[linSearch(operators,input.substring(i,i+3))]);
 		i+= 2;
 	    }
+	    //Searchs for ln
 	    else if (i < input.length() - 1 &&input.substring(i,i+2).equals("ln")) {
 		if (numberString.length() > 0) {
 		    equation.add(numberString);
@@ -30,6 +32,7 @@ public class Parser {
 		equation.add("ln");
 		i++;
 	    }
+	    // Searches for pi
 	    else if (i < input.length() - 1 &&input.substring(i,i+2).equals("pi")) {
                 if (numberString.length() > 0) {
                     equation.add(numberString);
@@ -38,30 +41,35 @@ public class Parser {
                 equation.add("pi");
                 i++;
             }
-	    else if (input.substring(i,i+1).equals("pi")) {
+            // Searches for e
+	    else if (input.substring(i,i+1).equals("e")) {
 		if (numberString.length() > 0) {
                     equation.add(numberString);
                     numberString = "";
                 }
                 equation.add("e");
 	    }
-
+	    // Searches for numbers if it is a number add to numberString
             else if (linSearch(numbers, input.substring(i,i+1)) != -1) {
                 numberString = numberString + input.substring(i,i+1);
             }
+            // If it isnt a number and numberString isnt empty than add numberString then add the operator
             else if (numberString.length() != 0) {
                 equation.add(numberString);
                 equation.add(input.substring(i,i+1));
                 numberString = "";
             }
+            // If not an operator and numberString is empty just add operator
 	    else {
 		equation.add(input.substring(i,i+1));
 	    }
+	    // If number occurs at end of input String
 	    if (input.length() - 1 == i && linSearch(numbers,input.substring(i,i+1)) !=  -1) {
 		equation.add(numberString);
 	    }
         }
     }
+    // Prints out elements of equation
     public String toString() {
 	String retString = "";
 	for (int i = 0; i < equation.size(); i++) {
@@ -110,7 +118,7 @@ public class Parser {
         return tPos;
     }
 
-    // LinSearch for arrayList
+    // LinSearch for ArrayList
     public static int linSearch ( ArrayList<String> a, String target ) {
 
         int tPos = -1;
@@ -125,6 +133,7 @@ public class Parser {
         }
         return tPos;
     }
+    
     public static int linSearch ( ArrayList<Double> a, Double target ) {
 
         int tPos = -1;
@@ -139,11 +148,12 @@ public class Parser {
         }
         return tPos;
     }
-
+    // simplifystep takes a sub ArrayList from equation without any parenthesis returns a string to be added back into equation
     public String simplifyStep(ArrayList<String> step) {
 	int size = step.size();
 	while (size > 1) {
 	    for (int i = 1; i < step.size(); i++) {
+	    	// Checks to see if two numbers are side by side if so multiply. ie coefficients
 		try {
 		    Double a = Double.parseDouble(step.get(i-1));
 		    Double b = Double.parseDouble(step.get(i));
@@ -155,6 +165,7 @@ public class Parser {
 		    continue;
 		}
 	    }
+	    // searches for each operator with respect to order of operations
 	    if (linSearch(step, "log") != -1) {
                 int i = linSearch(step, "log");
                 double answer = Math.log10(Double.parseDouble(step.get(i+1)));
@@ -256,11 +267,12 @@ public class Parser {
     }
     
     public Double simplify(double xcor) {
-	
+	// creates a duplicate so that equation is never changed
 	ArrayList<String> retList= new ArrayList<String>();
 	for (int i = 0; i < equation.size(); i++) {
 	    retList.add(i,equation.get(i));
 	}
+	// substitutes variables and constancts for appropriate values
 	for (int i = 0; i < equation.size(); i++) {
 	    if (equation.get(i).equals("x")) {
 		equation.set(i, Double.toString(xcor));
@@ -296,11 +308,13 @@ public class Parser {
 	    for (int i = 0; i < retList.size(); i++) {
 		equation.add(i,retList.get(i));
 	    }
+	    // if a real solution
 	    if (retDouble != Double.POSITIVE_INFINITY && retDouble != Double.NEGATIVE_INFINITY) {
 	       return retDouble;
 	    }
 	    return retDouble;
 	}
+	// imaginary solutions return null
 	catch (ArithmeticException ae) {
 	    return null;
 	}
@@ -309,9 +323,11 @@ public class Parser {
     public void findImportant(double a, double b, double interval) {
 	double starter = a;
 	while (a < b) {
+	    //if no real solution continue
 	    if (simplify(a).equals(Double.NaN)) {
 		a+= interval;
 	    }
+	    // else take sign and record
 	    else {
 		if (starter != a) {important.add(a);}
 		double sign = Math.signum(simplify(a));
@@ -322,11 +338,13 @@ public class Parser {
 			if (simplify(a) == 0) {important.add(a);}
 			newSign= Math.signum(simplify(a));
 		    }
+		    // Change of sign means cross x-axis or asymptote
 		    else {
 			sign = newSign;
 			if (simplify(a) != Double.NaN) {important.add(a); }
 		    }
 		}
+		// Refine solutions
 		int importantSize = important.size();
 		for (int i = 0; i < importantSize; i++) {
 		    double accurate = -interval / 10.;
@@ -351,7 +369,8 @@ public class Parser {
 			important.add(i,startValue);
 			counter++;
 		    }
-		}                     
+		}  
+		// decide whether solution is a root or an asymptote and add to appropriate ArrayList
 		for (int r = 0; r < important.size(); r++) {
 		    if (simplify(important.get(r)) == 0) {roots.add(important.get(r));}
 		    else if (Math.abs(simplify(important.get(r))) > 50) {
@@ -365,7 +384,7 @@ public class Parser {
 	    }
 	}
     }
-	
+    // returns roots in given interval [a,b]
     public ArrayList<Double> getRoots(double a, double b) {
 	int importantSize = important.size();
 	int rootSize = roots.size();
@@ -380,6 +399,7 @@ public class Parser {
 	findImportant(a,b,.1);
 	return roots;
     }
+    // returns asymptotes in given interval [a,b]
     public ArrayList<Double> getAsymptotes(double a,double b) {
 	int importantSize = important.size();
 	int asymptoteSize = asymptotes.size();
